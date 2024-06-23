7b2276c564e35e7743221a1fbeffca8d606d86b9
Denis Grudinin <denisgrudininworks@gmail.com>
2024-06-23 17:57:57 +0300

    upd: emoji carousel added

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 
 const FontWeight mediumFontWeight = FontWeight.w600;
+const FontWeight boldFontWeight = FontWeight.bold;
 
 const Color bgLight = Color.fromARGB(255, 213, 209, 253);
 const Color bgMidLight = Color.fromARGB(255, 235, 236, 255);
 const Color whiteColor = Colors.white;
+const Color blackColor = Colors.black;
+const Color blueColor = Color.fromARGB(255, 107, 78, 255);
 const Color transparentColor = Colors.transparent;
 
 const Gradient bgLightGradient = LinearGradient(
@@ -17,3 +20,15 @@ const Gradient bgLightGradient = LinearGradient(
     whiteColor,
   ],
 );
+
+const List<String> emojiList = [
+  'assets/emoji/happy.png',
+  'assets/emoji/sad.png',
+  'assets/emoji/angry.png',
+];
+
+const List<String> emojiNames = [
+  'Happy',
+  'Sad',
+  'Angry',
+];
--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,8 +1,17 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:mood_journal/constants.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  CarouselController emojiController = CarouselController();
+  int currentEmojiIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -11,30 +20,99 @@ class HomePage extends StatelessWidget {
         decoration: const BoxDecoration(
           gradient: bgLightGradient,
         ),
-        child: const Center(
+        child: Center(
           child: Padding(
-            padding: EdgeInsets.only(
-              top: 100,
+            padding: const EdgeInsets.only(
+              top: 90,
             ),
             child: Column(
               children: [
+                const Text(
+                  "What's your\nmood now?",
+                  style: TextStyle(
+                    fontSize: 48,
+                    fontWeight: mediumFontWeight,
+                    color: whiteColor,
+                  ),
+                ),
+
+                // Add Smiles Carousel
                 Expanded(
-                  child: Text(
-                    "What's your\nmood now?",
-                    style: TextStyle(
-                      fontSize: 48,
-                      fontWeight: mediumFontWeight,
-                      color: whiteColor,
+                  child: CarouselSlider.builder(
+                    carouselController: emojiController,
+                    itemCount: emojiList.length,
+                    itemBuilder: (context, itemIndex, _) {
+                      return Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            emojiList[itemIndex],
+                            height: 200,
+                            width: 200,
+                          ),
+                          currentEmojiIndex == itemIndex
+                              ? SizedBox(
+                                  height: 35,
+                                  child: Text(
+                                    emojiNames[itemIndex],
+                                    style: const TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: mediumFontWeight,
+                                      color: blackColor,
+                                    ),
+                                  ),
+                                )
+                              : const SizedBox(
+                                  height: 35,
+                                ),
+                        ],
+                      );
+                    },
+                    options: CarouselOptions(
+                      height: 500,
+                      viewportFraction: 0.5,
+                      initialPage: 0,
+                      enableInfiniteScroll: true,
+                      enlargeCenterPage: true,
+                      enlargeFactor: 0.5,
+                      scrollDirection: Axis.horizontal,
+                      autoPlay: true,
+                      autoPlayInterval: const Duration(milliseconds: 2000),
+                      autoPlayAnimationDuration:
+                          const Duration(milliseconds: 1200),
+                      autoPlayCurve: Curves.fastOutSlowIn,
+                      onPageChanged: (itemIndex, _) {
+                        setState(() {
+                          currentEmojiIndex = itemIndex;
+                        });
+                      },
                     ),
                   ),
                 ),
-                // Add Smiles Carousel
-                Expanded(
-                  child: SizedBox(),
-                ),
+
                 // Add Continue Button
-                Expanded(
-                  child: SizedBox(),
+                Padding(
+                  padding: const EdgeInsets.only(
+                    bottom: 90,
+                  ),
+                  child: Container(
+                    width: 330,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      color: blueColor,
+                    ),
+                    child: const Center(
+                      child: Text(
+                        "Continue",
+                        style: TextStyle(
+                          color: whiteColor,
+                          fontSize: 16,
+                          fontWeight: mediumFontWeight,
+                        ),
+                      ),
+                    ),
+                  ),
                 ),
               ],
             ),
